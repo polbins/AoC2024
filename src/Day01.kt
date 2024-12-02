4cdb4f88116c1f566a5e4a0073af220989ed9181
polbins <polbins@users.noreply.github.com>
2024-12-01 21:55:51 -0800

    01-2

--- src/Day01.kt
+++ src/Day01.kt
@@ -22,15 +22,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val rightCount = mutableMapOf<Int, Int>()
+
+        input.forEach { line ->
+            val split = line.split(WHITESPACE_REGEX)
+            left.add(split[0].toInt())
+            val right = split[1].toInt()
+            rightCount[right] = rightCount.getOrPut(right) { 0 } + 1
+        }
+
+        return input.indices.sumOf {
+            val num = left[it]
+            num * (rightCount[num] ?: 0)
+        }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
