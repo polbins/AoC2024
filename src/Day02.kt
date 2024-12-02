5d5df91fbad048939d440d9885b737e738479b29
polbins <polbins@users.noreply.github.com>
2024-12-02 12:30:02 -0800

    02-1, take 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,14 @@
 import kotlin.math.abs
 
-private fun Int.isPositive() = this > 0
+private fun Pair<Int?, Int>.safe(): Boolean {
+    val (previous, current) = this
+    return when {
+        abs(current) !in 1..3 -> false
+        (previous ?: 1) > 0 && current > 0 -> true
+        (previous ?: -1) < 0 && current < 0 -> true
+        else -> false
+    }
+}
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -11,26 +19,17 @@ fun main() {
 
         var result = 0
         lines.forEach { line ->
-            // subtract from left to right
-            // it should either be positive or negative all throughout, and difference should be 1 to 3
             var previousDifference: Int? = null
-            val safe = line.reduce { left, right ->
+            line.reduce { left, right ->
                 val difference = left - right
-                if (abs(difference) !in 1..3) {
-                    return@reduce -1
-                } else {
-                    when {
-                        previousDifference == null -> right
-                        previousDifference?.isPositive() == true && difference.isPositive() -> right
-                        previousDifference?.isPositive() == false && !difference.isPositive() -> right
-                        else -> return@reduce -1
-                    }.also {
-                        previousDifference = difference
-                    }
+                if (!(previousDifference to difference).safe()) {
+                    return@forEach
                 }
+
+                previousDifference = difference
+                right
             }
-            println(safe)
-            result += if (safe.isPositive()) 1 else 0
+            result += 1
         }
 
         return result
@@ -48,7 +47,7 @@ fun main() {
     check(part1(testInput) == 2)
 
     // Read the input from the `src/Day02.txt` file.
-//    val input = readInput("Day02")
-//    part1(input).println()
+    val input = readInput("Day02")
+    part1(input).println()
 //    part2(input).println()
 }
