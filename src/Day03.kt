9585aa8dac57ba066d186dfd1e68299ef795bde3
polbins <polbins@users.noreply.github.com>
2024-12-04 00:17:14 -0800

    03-2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,4 +1,5 @@
 private val MULTIPLICATION_REGEX = "mul\\((\\d+),(\\d+)\\)".toRegex()
+private val DISABLED_ENABLED_MULTIPLICATION_REGEX = "don't\\(\\)|do\\(\\)|mul\\((\\d+),(\\d+)\\)".toRegex()
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -10,7 +11,24 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        var enabled = true
+        input.forEach { line ->
+            DISABLED_ENABLED_MULTIPLICATION_REGEX.findAll(line).forEach {
+                with(it.value) {
+                    if (startsWith("don't")) {
+                        enabled = false
+                    } else if (startsWith("do")) {
+                        enabled = true
+                    } else {
+                        if (enabled) {
+                            result += it.groupValues[1].toInt() * it.groupValues[2].toInt()
+                        }
+                    }
+                }
+            }
+        }
+        return result
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -18,10 +36,10 @@ fun main() {
 
     // Or read a large test input from the `src/Day03_test.txt.txt` file:
 //    val testInput = readInput("Day03_test")
-//    check(part1(testInput) == 161)
+//    check(part2(testInput) == 48)
 
     // Read the input from the `src/Day03.txt` file.
     val input = readInput("Day03")
-    part1(input).println()
-//    part2(input).println()
+//    part1(input).println()
+    part2(input).println()
 }
